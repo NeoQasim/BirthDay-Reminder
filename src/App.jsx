103daf23a52cdb5807b2cc87722f0db8ea671b7a
NeoQasim <139628391+NeoQasim@users.noreply.github.com>
2023-08-24 23:14:26 +0500

    change case options

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import BirthdayRemind from './BirthdayRemind';
-// import Changecase from "./Changecase";
+import Changecase from './Changecase';
 // import Counter from "./Counter";
 
 function App() {
   return (
     <>
       {/* <Counter /> */}
-      {/* <Changecase /> */}
+      <Changecase />
       <BirthdayRemind />
     </>
   );
